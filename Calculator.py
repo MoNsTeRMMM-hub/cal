ba61c54ef36fe34dceaec5d8c3d3ccb1631075ee
MoNsTeRMMM-hub <58849994+MoNsTeRMMM-hub@users.noreply.github.com>
2019-12-14 22:29:09 +0200

    Calculator.py
    I don't know

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,17 @@
-#python 2.7.15
-
-a = int(input("enter (a)"))
-b = int(input("enter (b)"))
-c = int(input("enter (c)"))
+import math
+a = int(input("enter (a): "))
+b = int(input("enter (b): "))
+c = int(input("enter (c): "))
 delta = b*b - 4 * a * c
 dstr = str(delta)
 
-print(dstr)
+print("Δ= " +  dstr)
+
+num = int(input("please enter Δ again: "))
+suqrt = math.sqrt(num)
+sqrt = str(suqrt)
+print ("√Δ= " + sqrt)
+sfqrt = float(delta)
+first_x = - 1 * b + sfqrt \ 2 * a
+second_x = - 1 * b -  sfqrt \ 2 * a
+print (first_x + "  " + second_x)
